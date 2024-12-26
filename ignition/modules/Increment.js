385bb06793ec8bfbd1d6e4bc602ef53d7a24f113
Juan Bermudez <juanbermudeznunez@gmail.com>
2024-12-26 22:44:14 +0100

    add increment contract

---
+++ ignition/modules/Increment.js
@@ -0,0 +1,9 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+const incrementModule = buildModule("Increment", (m) => {
+    const increment = m.contract("Increment", [5, "counter"]);
+
+    return { increment };
+});
+
+module.exports = incrementModule;
