e510a06ec2934e8b5429fd8d74e5f38a83892815
Juan Bermudez <juanbermudeznunez@gmail.com>
2024-12-27 10:41:51 +0100

    Add details to README

--- contracts/Increment.sol
+++ contracts/Increment.sol
@@ -1,16 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import {console} from "hardhat/console.sol";
 
 contract Increment {
 
     address owner;
-
     struct Counter {
         uint256 number;
         string description;
     }
-
     Counter counter;
 
     modifier  onlyOwner {
@@ -19,14 +16,11 @@ contract Increment {
     }
 
     constructor(uint256 initial_value, string memory description){
-        console.log("increment constructor called with initial value %s and description %s", initial_value, description);
         owner = msg.sender;
         counter = Counter(initial_value, description);
-        console.log("Counter initialized with value %s and description %s", counter.number, counter.description);
     }
 
         // Since these funcions modify data on chain, they require gas
-
     function increment() external onlyOwner {
         counter.number += 1;
     }
@@ -34,9 +28,8 @@ contract Increment {
     function decrement() external onlyOwner {
         counter.number -= 1;
     }
-
-    function get() public returns (uint256) {
-        console.log("get called with value %s", counter.number);
+    
+    function getCounter() public view returns (uint256) {
         return counter.number;
     }
 }
