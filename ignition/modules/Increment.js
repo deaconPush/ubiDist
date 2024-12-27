56658668f4687f3102902e674af6c08d7b769f9e
Juan Bermudez <juanbermudeznunez@gmail.com>
2024-12-27 10:42:13 +0100

    lint files manually

--- ignition/modules/Increment.js
+++ ignition/modules/Increment.js
@@ -2,6 +2,7 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 const incrementModule = buildModule("Increment", (m) => {
     const increment = m.contract("Increment", [5, "counter"]);
+    
     return { increment };
 });
 
