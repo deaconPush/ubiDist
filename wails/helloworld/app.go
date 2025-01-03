e2343b709ec8a88ee55cafa3aa1f52e2fa10a72f
Juan Bermudez <juanbermudeznunez@gmail.com>
2025-01-03 19:27:29 +0100

    play around with helloWorld app

--- wails/helloworld/app.go
+++ wails/helloworld/app.go
@@ -3,7 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"encoding/json"
+//	"log"
 )
+
 
 // App struct
 type App struct {
@@ -24,4 +29,28 @@ func (a *App) startup(ctx context.Context) {
 // Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+func (a *App) GenerateWord() (string, error) {
+	// Make the GET request
+	resp, err := http.Get("https://random-word-api.herokuapp.com/word")
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return "", err	
+	}
+
+	var words []string
+	err = json.Unmarshal(body, &words)
+
+	if err != nil {
+		return "", err
+	}	
+	// log.Println("word: ", words[0])
+	return words[0], nil
 }
