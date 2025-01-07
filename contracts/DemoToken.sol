499cafb4dff3486fac5d930241a1fa400c8b3eb7
Juan Bermudez <juan.nunez@staffbase.com>
2025-01-07 17:12:09 +0100

    Add interaction script with ECR20 token

--- contracts/DemoToken.sol
+++ contracts/DemoToken.sol
@@ -5,9 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
 contract DemoToken is ERC20 {
+
+        uint256 constant initialSupply = 1000000 * (10**18);
+
     // DemoToken is the name of the contract and it inherits from ERC20
     constructor() ERC20("DemoToken", "DT") {
         // The mint function mins an initial supply of tokens
-        _mint(msg.sender, 1000000 * 10 ** decimals());
+        _mint(msg.sender, initialSupply);
     }
 }
