b5156675044d5d6f01c45516b99d50b8e8b93b4e
deaconPush <64878517+deaconPush@users.noreply.github.com>
2025-01-17 18:00:37 +0100

    Explore hardhat RPC API

--- wails/wallet/app.go
+++ wails/wallet/app.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"context"
-	"fmt"
+	"encoding/json"
+	"io"
+	"log"
+	"math/big"
+	"net/http"
+
+	"github.com/google/uuid"
 )
 
 // App struct
@@ -21,7 +28,50 @@ func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+func getBalance(provider string, address string) string {
+	payload := map[string]interface{}{
+		"jsonrpc": "2.0",
+		"method":  "eth_getBalance",
+		"params":  []interface{}{address, "latest"},
+		"id":      uuid.New().String(),
+	}
+
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Fatal(err)
+		return ""
+	}
+
+	respBytes, err := http.Post(provider, "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		log.Fatal(err)
+		return ""
+	}
+
+	defer respBytes.Body.Close()
+
+	body, err := io.ReadAll(respBytes.Body)
+	if err != nil {
+		log.Fatal(err)
+		return ""
+	}
+
+	response := map[string]interface{}{}
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		log.Fatal(err)
+		return ""
+	}
+
+	balanceHex := response["result"].(string)
+	balance := weiToEther(balanceHex)
+	return balance
+}
+
+func weiToEther(wei string) string {
+	weiBigInt := new(big.Int)
+	weiBigInt.SetString(wei, 0)
+	ether := new(big.Float).SetInt(weiBigInt)
+	ether = new(big.Float).Quo(ether, big.NewFloat(1e18))
+	return ether.String()
 }
--- wails/wallet/main.go
+++ wails/wallet/main.go
@@ -2,10 +2,6 @@ package main
 
 import (
 	"embed"
-
-	"github.com/wailsapp/wails/v2"
-	"github.com/wailsapp/wails/v2/pkg/options"
-	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
 //go:embed all:frontend/dist
@@ -13,24 +9,25 @@ var assets embed.FS
 
 func main() {
 	// Create an instance of the app structure
-	app := NewApp()
+	//app := NewApp()
+	getBalance("http://127.0.0.1:8545/", "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
 
 	// Create application with options
-	err := wails.Run(&options.App{
-		Title:  "wallet",
-		Width:  1024,
-		Height: 768,
-		AssetServer: &assetserver.Options{
-			Assets: assets,
-		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
-		Bind: []interface{}{
-			app,
-		},
-	})
+	// err := wails.Run(&options.App{
+	// 	Title:  "wallet",
+	// 	Width:  1024,
+	// 	Height: 768,
+	// 	AssetServer: &assetserver.Options{
+	// 		Assets: assets,
+	// 	},
+	// 	BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+	// 	OnStartup:        app.startup,
+	// 	Bind: []interface{}{
+	// 		app,
+	// 	},
+	// })
 
-	if err != nil {
-		println("Error:", err.Error())
-	}
+	// if err != nil {
+	// 	println("Error:", err.Error())
+	// }
 }
