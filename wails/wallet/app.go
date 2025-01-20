dda52e4fc1cc4a14c3d17dda8bb410c948724703
Juan Bermudez <juanbermudeznunez@gmail.com>
2025-01-20 22:26:14 +0100

    Commit broken transaction workflow

--- wails/wallet/app.go
+++ wails/wallet/app.go
@@ -3,18 +3,34 @@ package main
 import (
 	"bytes"
 	"context"
+	"crypto/ecdsa"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"math/big"
 	"net/http"
 
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/google/uuid"
 )
 
 // App struct
 type App struct {
 	ctx context.Context
+}
+
+type Transaction struct {
+	Nonce    uint64
+	GasPrice *big.Int
+	GasLimit uint64
+	To       string
+	Value    *big.Int
+	Data     []byte
+	V        *big.Int
+	R        *big.Int
+	S        *big.Int
 }
 
 type RPCPayload struct {
@@ -35,7 +51,7 @@ func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
-func getGasPrice(provider string) string {
+func getGasPrice(provider string) *big.Int {
 	payload := RPCPayload{
 		Jsonrpc: "2.0",
 		Method:  "eth_gasPrice",
@@ -45,10 +61,11 @@ func getGasPrice(provider string) string {
 
 	response := sendRPCRequest(payload, provider)
 	gasPriceHex := response["result"].(string)
-	return gasPriceHex
+	gasPrice, _ := new(big.Int).SetString(gasPriceHex[2:], 16)
+	return gasPrice
 }
 
-func getTransactionCount(provider string, address string) string {
+func getNonce(provider string, address string) uint64 {
 	payload := RPCPayload{
 		Jsonrpc: "2.0",
 		Method:  "eth_getTransactionCount",
@@ -57,8 +74,117 @@ func getTransactionCount(provider string, address string) string {
 	}
 
 	response := sendRPCRequest(payload, provider)
-	transactionCountHex := response["result"].(string)
-	return transactionCountHex
+	nonceHex := response["result"].(string)
+	nonce := new(big.Int)
+	nonce.SetString(nonceHex[2:], 16)
+	return nonce.Uint64()
+}
+
+func estimateGas(provider string, from string, to string, value *big.Int) uint64 {
+	payload := RPCPayload{
+		Jsonrpc: "2.0",
+		Method:  "eth_estimateGas",
+		Params: []interface{}{
+			map[string]interface{}{
+				"from":  from,
+				"to":    to,
+				"value": value,
+			},
+		},
+		ID: uuid.New().String(),
+	}
+
+	response := sendRPCRequest(payload, provider)
+	gasLimitHex := response["result"].(string)
+	gasLimit := new(big.Int)
+	gasLimit.SetString(gasLimitHex[2:], 16)
+	return gasLimit.Uint64()
+}
+
+func signTransaction(tx *Transaction, privateKey *ecdsa.PrivateKey, chainID *big.Int) ([]byte, error) {
+	txRLP := []interface{}{
+		tx.Nonce,
+		tx.GasPrice,
+		tx.GasLimit,
+		tx.To,
+		tx.Value,
+		tx.Data,
+		chainID, big.NewInt(0), big.NewInt(0),
+	}
+
+	encodedTx, err := rlp.EncodeToBytes(txRLP)
+	if err != nil {
+		return []byte{}, fmt.Errorf("failed to RLP encode transaction: %w", err)
+	}
+
+	txHash := crypto.Keccak256(encodedTx)
+	signature, err := crypto.Sign(txHash, privateKey)
+	fmt.Println("signature:", signature)
+	if err != nil {
+		return []byte{}, fmt.Errorf("failed to sign transaction: %w", err)
+	}
+
+	r := new(big.Int).SetBytes(signature[0:32])
+	s := new(big.Int).SetBytes(signature[32:64])
+	v := big.NewInt(int64(signature[64]) + 35 + 2*chainID.Int64())
+	// Add the signature to the transaction
+	tx.V = v
+	tx.R = r
+	tx.S = s
+	// RPL encode the transaction with the signature
+	signedTxRLP := []interface{}{
+		tx.Nonce,
+		tx.GasPrice,
+		tx.GasLimit,
+		tx.To,
+		tx.Value,
+		tx.Data,
+		tx.V,
+		tx.R,
+		tx.S,
+	}
+	fmt.Println("Signing transaction with appropiate data...")
+	signedEncodedTx, err := rlp.EncodeToBytes(signedTxRLP)
+	fmt.Println("signedEncodedTx:", signedEncodedTx)
+	if err != nil {
+		return []byte{}, fmt.Errorf("failed to RLP encode signed transaction: %w", err)
+	}
+
+	return signedEncodedTx, nil
+}
+
+func processTransaction(provider string, from string, to string, value *big.Int, privateKey *ecdsa.PrivateKey, chainID int64) string {
+	nonce := getNonce(provider, from)
+	gasPrice := getGasPrice(provider)
+	gasLimit := estimateGas(provider, from, to, value)
+	tx := Transaction{
+		Nonce:    nonce,
+		GasPrice: gasPrice,
+		GasLimit: gasLimit,
+		To:       to,
+		Value:    value,
+		Data:     nil,
+	}
+
+	fmt.Print("Signing transaction...")
+	signedTx, err := signTransaction(&tx, privateKey, big.NewInt(chainID))
+	if err != nil {
+		log.Fatal("Error signing transaction :", err)
+	}
+
+	payload := RPCPayload{
+		Jsonrpc: "2.0",
+		Method:  "eth_sendRawTransaction",
+		Params:  []interface{}{signedTx},
+		ID:      uuid.New().String(),
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	response := sendRPCRequest(payload, provider)
+	fmt.Println("response:", response)
+	return ""
 }
 
 func sendRPCRequest(payload RPCPayload, url string) map[string]interface{} {
--- wails/wallet/go.mod
+++ wails/wallet/go.mod
@@ -1,6 +1,6 @@
 module wallet
 
-go 1.21
+go 1.22
 
 toolchain go1.23.4
 
@@ -8,9 +8,19 @@ require github.com/wailsapp/wails/v2 v2.9.2
 
 require (
 	github.com/bep/debounce v1.2.1 // indirect
-	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/bits-and-blooms/bitset v1.13.0 // indirect
+	github.com/consensys/bavard v0.1.13 // indirect
+	github.com/consensys/gnark-crypto v0.12.1 // indirect
+	github.com/crate-crypto/go-ipa v0.0.0-20240223125850-b1e8a79f509c // indirect
+	github.com/crate-crypto/go-kzg-4844 v1.0.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/ethereum/c-kzg-4844 v1.0.0 // indirect
+	github.com/ethereum/go-ethereum v1.14.12 // indirect
+	github.com/ethereum/go-verkle v0.1.1-0.20240829091221-dffa7562dbe9 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/holiman/uint256 v1.3.1 // indirect
 	github.com/jchv/go-winloader v0.0.0-20210711035445-715c2860da7e // indirect
 	github.com/labstack/echo/v4 v4.10.2 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
@@ -19,21 +29,25 @@ require (
 	github.com/leaanthony/slicer v1.6.0 // indirect
 	github.com/leaanthony/u v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mmcloughlin/addchain v0.4.0 // indirect
 	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/samber/lo v1.38.1 // indirect
+	github.com/supranational/blst v0.3.13 // indirect
 	github.com/tkrajina/go-reflector v0.5.6 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/wailsapp/go-webview2 v1.0.16 // indirect
 	github.com/wailsapp/mimetype v1.4.1 // indirect
 	golang.org/x/crypto v0.23.0 // indirect
-	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
+	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa // indirect
 	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
+	rsc.io/tmplfunc v0.0.3 // indirect
 )
 
 // replace github.com/wailsapp/wails/v2 v2.9.2 => /home/juampi/go/pkg/mod
