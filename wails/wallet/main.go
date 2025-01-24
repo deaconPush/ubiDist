9cc0d909a01fd9b083ce5a5cab6c0553eb1bcd33
Juan Bermudez <juanbermudeznunez@gmail.com>
2025-01-24 12:23:33 +0100

    Add basic UI to wallet project

--- wails/wallet/main.go
+++ wails/wallet/main.go
@@ -17,12 +17,12 @@ func main() {
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "wallet",
-		Width:  1024,
-		Height: 768,
+		Width:  600,
+		Height: 600,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		BackgroundColour: &options.RGBA{R: 173, G: 255, B: 47, A: 255},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
