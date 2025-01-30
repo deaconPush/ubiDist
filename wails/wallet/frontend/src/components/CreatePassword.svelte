c298b220098240450baabf049833cca5fca2fce3
Juan Bermudez <juan.nunez@staffbase.com>
2025-01-30 14:07:38 +0100

    Modularize and utilize components

---
+++ wails/wallet/frontend/src/components/CreatePassword.svelte
@@ -0,0 +1,195 @@
+<script>
+      export let handleClick = () => {};
+      export let walletLabel = '';
+
+    let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{12,}$/;
+    function isPasswordValid(password) {
+      return passwordRegex.test(password);
+    }
+  
+    function checkPasswordStrength() {
+      const password = document.getElementById('wallet-password').value;
+      const passwordStrengthContainer = document.getElementById('password-strength');
+      const isPasswordStrong = isPasswordValid(password);
+      passwordStrengthContainer.style.display = 'block';
+      if (isPasswordStrong) {
+        passwordStrengthContainer.textContent = 'Password strength: Strong';
+       } else {
+          passwordStrengthContainer.textContent = 'Password strength: Weak';
+        }
+      }
+  
+    function checkPasswordsMatch() {
+      const password = document.getElementById('wallet-password').value;
+      const confirmPassword = document.getElementById('confirm-password').value;
+      const isPasswordStrong = isPasswordValid(password);
+      const createWalletButton = document.getElementById('create-wallet-button');
+      const passwordMatchContainer = document.getElementById('password-match');
+      passwordMatchContainer.style.display = 'block';
+      if (password !== confirmPassword) {
+        passwordMatchContainer.textContent = 'Passwords do not match';
+      } else {
+        passwordMatchContainer.textContent = '';
+        if (isPasswordStrong) {
+          createWalletButton.disabled = false;
+          createWalletButton.style.backgroundColor = 'black';
+        }
+      }
+    }   
+    </script>
+    
+<div class="header-content">
+  <h2>Create Password</h2>
+  <p>This password will unlock your wallet on this application.</p>
+</div>
+<form class="form-container">
+  <div class="input-group">
+    <label for="password">New password (16 characters min, at least one character should be in lowercase, one in uppercase and one number)</label>
+    <input type="password" id="wallet-password" on:input={checkPasswordStrength} />
+    <p id="password-strength" class="validation-label" ></p>
+  </div>
+  <div class="input-group">
+    <label for="confirm-password">Confirm password</label>
+    <input type="password" id="confirm-password" on:input={checkPasswordsMatch} />
+    <p id="password-match" class="validation-label" ></p>
+  </div>
+  <button id="wallet-button" type="button" disabled=true on:click={handleClick}>{walletLabel}</button>
+</form>
+    
+    <style>
+    .header-content {
+      text-align: center;
+      margin-bottom: 20px;
+    }
+  
+    h2 {
+      font-size: 2rem;
+      color: #0066cc; 
+      margin-bottom: 10px;
+    }
+  
+    p {
+      font-size: 1rem;
+      color: #555; 
+      margin-bottom: 20px;
+    }
+  
+    /* Form styles */
+    .form-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      align-content: center;
+      gap: 20px;
+      width: 100%;
+      max-width: 400px;
+      background-color: #fff;
+      padding: 20px;
+      border-radius: 10px;
+      box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1); 
+    }
+  
+    .input-group {
+      display: flex;
+      flex-direction: column;
+      gap: 2px;
+      align-items: center;
+    }
+  
+    label {
+      font-size: 1rem;
+      font-weight: bold;
+      color: #222;
+    }
+  
+    input {
+      padding: 4px;
+      font-size: 1rem;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      max-width: 190px;
+      color: #333; 
+      background-color: #fefefe; 
+    }
+  
+    input:focus {
+      outline: none;
+      border-color: #0066cc; 
+      box-shadow: 0 0 3px #0066cc;
+    }
+  
+    #create-wallet-button {
+      text-align: center;
+      color: white;
+      background-color: #0066cc;
+      font-family: "Nunito", sans-serif;
+      font-weight: bold;
+      width: 100%;
+      height: 25px;
+      border-radius: 25px;
+      cursor: pointer;
+      border: none;
+      transition: background-color 0.3s ease;
+    }
+  
+    #create-wallet-button:hover:not(:disabled) {
+      background-color: #004c99; 
+    }
+  
+    #create-wallet-button:disabled {
+      background-color: #ccc; 
+      color: #666;
+      cursor: not-allowed;
+    }
+  
+    /* Validation messages */
+    .validation-label {
+      font-size: 0.9rem;
+      color: red; 
+      display: none; 
+    }
+  
+    /* Responsive styles */
+    @media (max-width: 768px) {
+      main {
+        padding: 15px;
+      }
+  
+      h2 {
+        font-size: 1.8rem;
+      }
+  
+      h3 {
+        font-size: 1.3rem;
+      }
+  
+      .form-container {
+        padding: 15px;
+      }
+  
+      #create-wallet-button {
+        height: 40px;
+      }
+    }
+  
+    @media (max-width: 480px) {
+      h2 {
+        font-size: 1.5rem;
+      }
+  
+      h3 {
+        font-size: 1.2rem;
+      }
+  
+      label,
+      input,
+      #create-wallet-button {
+        font-size: 0.9rem;
+      }
+  
+      .form-container {
+        gap: 15px;
+      }
+    }
+    </style>
+    
--- wails/wallet/frontend/src/views/RestoreWallet.svelte
+++ wails/wallet/frontend/src/views/RestoreWallet.svelte
@@ -1,38 +1,43 @@
 <script>
     import { ValidateMnemonic } from '../../wailsjs/go/main/App'    
+    import SeedRecovery from '../components/SeedRecovery.svelte';
+    import CreatePassword from '../components/CreatePassword.svelte';
+    
+    let seedPhrase = ''
+    let seedPhraseBlocks = 12;
+    let showSeedRecovery = true;
+
+    function restoreWallet() {
+        console.log('Restore wallet');
+    }
 
     function confirmRecoveryPhrase() {
         const inputs = document.querySelectorAll('.seed-phrase-block');
-        const seedPhrase = Array.from(inputs).map(input => input.value.trim()).join(' ');
+        seedPhrase = Array.from(inputs).map(input => input.value.trim()).join(' ');
         ValidateMnemonic(seedPhrase).then(isValid => {
             if(isValid) {
-                console.log('Recovery phrase is valid');
+                showSeedRecovery = false;
             } else {
                 console.log('Invalid recovery phrase');
             }
         })
     }
-
-    let seedPhraseBlocks = 12;
-    function checkSeedInputs() {
-        const inputs = document.querySelectorAll('.seed-phrase-block');
-        const confirmButton = document.getElementById('confirm-recovery-button');
-        const allFilled = Array.from(inputs).every(input => input.value.trim() !== '');
-        confirmButton.disabled = !allFilled;
-    }
 </script>
 
+
 <main>
-    <h3>Restore wallet</h3>
-    <h4>Enter the Secret Recovery Phrase that you were given when you created your wallet.</h4>
-    <div class="seed-words-container">
-        { #each Array(seedPhraseBlocks) as _, i }
-            <input on:input={checkSeedInputs} type="text" class="seed-phrase-block" />
-        { /each }
-    </div>
-    <div class="confirm-recovery-button-container">
-        <button id="confirm-recovery-button" on:click={confirmRecoveryPhrase} disabled=true>Confirm Recovery Phrase</button>
-    </div>
+    {#if showSeedRecovery}
+        <SeedRecovery 
+        seedPhraseBlocks={seedPhraseBlocks}
+        onConfirm={confirmRecoveryPhrase}
+        />
+    {:else}
+        <CreatePassword 
+            handleClick={restoreWallet}
+            walletLabel="Restore Wallet"
+        />
+    {/if}
+    
 </main>
 
 <style>
@@ -47,118 +52,5 @@ main {
     background-color: #f4f4f9; 
     font-family: "Nunito", sans-serif;
     flex-grow: 1;
-}
-
-h3, h4 {
-    margin-top: 20px;
-    color: #333; 
-    text-align: center; 
-    padding: 0 10px;
-}
-
-.seed-words-container {
-    display: grid;
-    grid-template-columns: repeat(3, minmax(120px, 1fr)); 
-    column-gap: 40px; 
-    row-gap: 15px; 
-    justify-items: center;
-    align-items: center;
-    margin-bottom: 20px;
-    width: 100%;
-    max-width: 600px; 
-}
-
-.seed-phrase-block {
-    width: 100%; 
-    font-size: 15px;
-    background-color: #fff; 
-    border: 1px solid #ccc;
-    border-radius: 7px;
-    height: 40px;
-    font-family: "Nunito";
-    padding: 0 10px;
-    color: #333; 
-}
-
-.seed-phrase-block::placeholder {
-    color: #999; 
-}
-
-.confirm-recovery-button-container {
-    display: flex;
-    justify-content: center;
-    width: 100%;
-}
-
-#confirm-recovery-button {
-    padding: 12px 20px;
-    margin-top: 40px;
-    background-color: #0066cc;
-    max-width: 300px;
-    font-family: "Nunito";
-    font-weight: bold;
-    color: #fff; 
-    border-radius: 20px;
-    border: none;
-    width: 100%;
-    cursor: pointer;
-    transition: background-color 0.3s ease;
-}
-
-#confirm-recovery-button:hover:not(:disabled) {
-    background-color: #004c99; 
-}
-
-#confirm-recovery-button:disabled {
-    background-color: #ccc; 
-    color: #666;
-    cursor: not-allowed;
-}
-
-@media (max-width: 1024px) {
-    .seed-words-container {
-        grid-template-columns: repeat(3, minmax(100px, 1fr));
-        column-gap: 40px; 
-    }
-
-    #confirm-recovery-button {
-        max-width: 250px;
-    }
-}
-
-@media (max-width: 768px) {
-    .seed-words-container {
-        grid-template-columns: repeat(3, minmax(80px, 1fr)); 
-        column-gap: 40px;
-    }
-
-    h3, h4 {
-        font-size: 1.2rem;
-        margin-top: 10px;
-    }
-
-    #confirm-recovery-button {
-        max-width: 200px;
-    }
-}
-
-@media (max-width: 480px) {
-    .seed-words-container {
-        grid-template-columns: repeat(3, minmax(80px, 1fr));
-        column-gap: 40px;
-    }
-
-    h3 {
-        font-size: 1.5rem;
-    }
-
-    h4 {
-        font-size: 1rem;
-    }
-
-    #confirm-recovery-button {
-        max-width: 100%; 
-        padding: 10px 15px;
-    }
 }
 </style>
