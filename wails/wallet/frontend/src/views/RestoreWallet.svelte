8224babe06a938c9744da2210b90e8991decd261
Juan Bermudez <juanbermudeznunez@gmail.com>
2025-02-03 15:01:47 +0100

    Modify existing views and components to use ts instead of js

--- wails/wallet/frontend/src/views/RestoreWallet.svelte
+++ wails/wallet/frontend/src/views/RestoreWallet.svelte
@@ -1,28 +1,41 @@
-<script>
+<script lang="ts">
     import { ValidateMnemonic, RestoreWallet } from '../../wailsjs/go/main/App'    
     import SeedRecovery from '../components/SeedRecovery.svelte';
     import CreatePassword from '../components/CreatePassword.svelte';
     
-    let seedPhrase = ''
-    let seedPhraseBlocks = 12;
-    let showSeedRecovery = true;
+    let seedPhrase: string = ''
+    let seedPhraseBlocks: number = 12;
+    let showSeedRecovery: boolean = true;
 
-    function restoreWallet() {
-        const password = document.getElementById('wallet-password').value;
-        RestoreWallet(password, seedPhrase).then((err) => {
-            if (err) {
-                console.log("Error restoring wallet: ", err);
-            }
+    function restoreWallet(): void {
+        const passwordInput = document.getElementById('wallet-password') as HTMLInputElement;
+        if (!passwordInput) {
+            console.error('Password input not found');
+            return;
+        }
+
+        const password = passwordInput.value;
+        RestoreWallet(password, seedPhrase)
+        .then(() => {
+            console.log("Wallet restored");
         })
+        .catch((error) => {
+            console.error(error);
+        });
     }
-    function confirmRecoveryPhrase() {
-        const inputs = document.querySelectorAll('.seed-phrase-block');
+    function confirmRecoveryPhrase(): void {
+        const inputs = document.querySelectorAll('.seed-phrase-block') as NodeListOf<HTMLInputElement>;
+        if (!inputs) {
+            console.error('Seed phrase inputs not found');
+            return;
+        }
+        
         seedPhrase = Array.from(inputs).map(input => input.value.trim()).join(' ');
         ValidateMnemonic(seedPhrase).then(isValid => {
             if(isValid) {
                 showSeedRecovery = false;
             } else {
-                
+                console.error('Invalid seed phrase');
             }
         })
     }
