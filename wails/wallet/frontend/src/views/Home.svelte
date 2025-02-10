ff71a5671e196602567cbb123cf8d840ef27ccbc
Juan Bermudez <juan.nunez@staffbase.com>
2025-02-10 18:36:37 +0100

    Finish first mock for home view

--- wails/wallet/frontend/src/views/Home.svelte
+++ wails/wallet/frontend/src/views/Home.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+    import ethLogo from '../assets/images/eth-logo.png';
     let balance: number = 0;
-    
+    let ethBalance: number = 180;
 </script>
 
 <main>
@@ -12,15 +13,19 @@
             <button id="receive-crypto-button">Receive</button>
         </div>
     </div>
-    <!-- <div class="assets-container">
-        <h1>Your assets</h1>
+    <div class="assets-container">
+        <h4 id="assets-title">Assets</h4>
         <div class="assets-list">
             <div class="asset">
                 <img src={ethLogo} alt="eth logo">
-                <h2>Ethereum</h2>
-                <h3>{ethBalance}</h3>
+                <div class="coin-description-container">
+                    <h6 class="coin-description-symbol">ETH</h6>
+                    <h5 class="coin-description-name">Ethereum</h5>
+                </div>
+                <h3 class="coin-balance">{ethBalance}</h3>
             </div>
-        </div> -->
+         </div>
+    </div>
 </main>
 
 
@@ -28,7 +33,7 @@
 
 main {
         font-family: "Nunito", sans-serif;
-        color: #333; 
+        color: black;
         background-color: #f9f9f9; 
         height: 100vh;
         padding: 20px;
@@ -40,6 +45,7 @@ main {
     .balance-container h4 {
         font-weight: bold;
         margin-top: -5px;
+        font-size: 1rem;
         margin-right: 300px;
 
     }
@@ -65,6 +71,7 @@ main {
         padding: 10px 15px;
         margin: 5px;
         width: 180px;
+        justify-content: center;
         border-radius: 8px;
         cursor: pointer;
         transition: background 0.3s;
@@ -73,5 +80,74 @@ main {
     .balance-buttons-container button:hover {
         background: #004c99;
     }
+
+    .assets-container {
+        margin-top: 15px;
+        width: 90%;
+        padding: 5px 20px;
+        max-width: 400px;
+        margin: 25px;
+        border-radius: 12px;
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+        background: #fefefe;
+        transition: background 0.3s;
+    }
+
+
+    #assets-title {
+        font-weight: bold;
+        margin-right: 350px;
+        margin-top: 5px;
+
+    
+    }
+
+    .assets-list {
+        padding: 1px;
+        margin-top: -8px;
+        border-radius: 12px;
+
+    }
+
+    .asset {
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        padding: 1px;
+        margin-top: 10px;
+        border-bottom: 1.5px solid #ccc;
+    }
+
+    .asset img {
+        width: 30px;
+        height: 30px;
+        margin-right: 10px;
+    }
+    
+    .coin-description-container {
+        margin-top: 15px;
+        display: block;
+    }
+
+    .coin-description-symbol {
+        position: relative;
+        font-size: 0.8rem;
+        margin: 0;
+        font-weight: bold;
+    }
+    .coin-description-name {
+        font-size: 0.9rem;
+        margin: 3px;
+        color: #666;
+    }
+
+    .coin-balance {
+        font-size: 1.1rem;
+        font-weight: bold;
+        margin-left:240px;
+    }
+
+  
+  
 
   </style>
